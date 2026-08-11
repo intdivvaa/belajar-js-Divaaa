1244aaf8846682720dac950a1d44b4c1307dfb33
divaa <dvalvvzie@gmail.com>
2026-08-11 16:54:58 +0700

    menyelesaikan tugasb01-biodata

--- 01-biodata/README.md
+++ 01-biodata/README.md
@@ -19,3 +19,7 @@ Hobi saya: futsal dan ngoding.
 > Tulis di sini (3–5 kalimat, bahasamu sendiri):
 > - Bagaimana caramu menyelesaikan tugas ini?
 > - Kenapa kamu memilih `const` untuk data tertentu dan `let` untuk data lainnya?
+
+// Saya nambahin variabel nama, umur, asal sekolah, dan hobi
+// terus pakai const di varianel nama, asal sekolah dan hobi, terus let di umur agar bisa berubah
+// dan agar dapat tampil di terminal pakai console.log, jadi hasilnya bisa muncul di terminal.
--- 01-biodata/index.js
+++ 01-biodata/index.js
@@ -1,7 +1,15 @@
 // Tugas 01 — Biodata Diri
 // Simpan data dirimu dalam variabel, lalu tampilkan kalimat perkenalan.
 
+const nama = "Intan Diva Arianti";
+let umur = 18;
+const asal = "SMKN 1 Pacitan";
+const hobi = "Mendengarkan musik";
+
 // TODO: buat variabel di sini (pikirkan mana yang const, mana yang let)
 
-
 // TODO: tampilkan kalimat perkenalan dengan console.log
+
+console.log(`Halo! Nama Saya ${nama}.`);
+console.log(`Umur saya ${umur} tahun, dari ${asal}.`);
+console.log(`Hobi Saya: ${hobi}.`);
